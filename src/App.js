0051de80d5ffa4cc36fa945875b3c36ccb4f8990
tonoanzzu <tono.anzzu@gmail.com>
2020-03-25 13:48:41 -0600

    Actualizacion de archivo App.js para practica de capitulo, si no crasheaba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React, {Fragment, Component, useState } from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+  function App(){
 
-class App extends Component {
+     const [productos, guardarProductos] = useState([
+      {id: 1, nombre: 'Camisa ReactJS', precio:50},
+      {id: 2, nombre: 'Camisa VueJS', precio:40},
+      {id: 3, nombre: 'Camisa NodeJS', precio:30},
+      {id: 4, nombre: 'Camisa Angular', precio:20}
+    ]); 
 
-  
-  render() {
     const fecha =  new Date().getFullYear();
 
     return (
-      <div className="App">
+      <Fragment>
+
+      
        <Header
        titulo="Tienda virtual"
        />
        <Footer fecha={fecha} />
-      </div>
+       </Fragment>
     );
+
   }
-}
+
 
 export default App;
