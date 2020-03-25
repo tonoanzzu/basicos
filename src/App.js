1c83b3ae76c7a1cc1786d894d3ce9033d556ee58
tonoanzzu <tono.anzzu@gmail.com>
2020-03-25 02:05:21 -0600

    Anexar footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Header, {  } from "./components/Header";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
        <Header />
+       <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Todos los derechos reservados &copy;</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
