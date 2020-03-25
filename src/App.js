02c785325ced5a68537c8819a9deb2d157715cf2
tonoanzzu <tono.anzzu@gmail.com>
2020-03-25 00:04:22 -0600

    Creacion de primer componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header, {  } from "./components/Header";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hola mundo</h1>
+       <Header />
       </div>
     );
   }
